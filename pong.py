c778ea3bc052db623b14e1039f3f5d2128e610fd
Daniel Naylor <naylordaniel@yahoo.com>
2020-01-09 13:07:39 -0800

    finished!

--- pong.py
+++ pong.py
@@ -34,6 +34,20 @@ ball.goto(0, 0)
 ball.dx = .2
 ball.dy = .2
 
+# Pen
+pen = turtle.Turtle()
+pen.speed(0)
+pen.color("white")
+pen.penup()
+pen.hideturtle()
+pen.goto(0, 260)
+pen.write("Player A: 0  Player B: 0", align="center", font=("Courier", 24, "normal"))
+
+
+# Score
+score_a = 0
+score_b = 0
+
 # functions
 def left_paddle_up():
     y = left_paddle.ycor()
@@ -70,3 +84,37 @@ while True:
     #move ball
     ball.setx(ball.xcor() + ball.dx)
     ball.sety(ball.ycor() + ball.dy)
+
+    # border checking
+    if ball.ycor() > 290:
+        ball.sety(290)
+        ball.dy *= -1
+    
+    if ball.ycor() < -290:
+        ball.sety(-290)
+        ball.dy *= -1
+
+    if ball.xcor() > 390:
+        ball.goto(0, 0)
+        ball.dx *= -1
+        score_a += 1
+        pen.clear()
+        pen.write(f'Player A: {score_a}  Player B: {score_b}', align="center", font=("Courier", 24, "normal"))
+
+    if ball.xcor() < -390:
+        ball.goto(0, 0)
+        ball.dx *= -1
+        score_b += 1
+        pen.clear()
+        pen.write(f'Player A: {score_a}  Player B: {score_b}', align="center", font=("Courier", 24, "normal"))
+
+    # paddle / ball collisions
+    if ball.xcor() > 340 and ball.xcor() < 350 and ball.ycor() < right_paddle.ycor() + 40 and ball.ycor() > right_paddle.ycor() - 40:
+        ball.setx(340)
+        ball.dx *= -1
+
+    if ball.xcor() < -340 and ball.xcor() > -350 and ball.ycor() < left_paddle.ycor() + 40 and ball.ycor() > left_paddle.ycor() - 40:
+        ball.setx(-340)
+        ball.dx *= -1
+
+    
